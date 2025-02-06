a7701b8f05b9ce7bf668328f128e35d52d9b2fec
akifyev <eee@yas.ru>
2025-02-06 18:53:34 +0300

    done

---
+++ Demo_Akifev_420_P/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Demo_Akifev_420_P.DB;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Demo_Akifev_420_P
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Demo_DB_Akifev_420_PEntities db = new Demo_DB_Akifev_420_PEntities();
+    }
+}
---
+++ Demo_Akifev_420_P/Pages/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Demo_Akifev_420_P.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo_Akifev_420_P.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public static List<Product> prod { get; set; }
+        public Product product;
+        public MainPage()
+        {
+            InitializeComponent();
+            prod = App.db.Product.ToList();
+            ProductsLw.ItemsSource = prod;
+        }
+
+        private void ProductsLw_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            product = ProductsLw.SelectedItem as Product;
+            NavigationService.Navigate(new EditPage());
+
+        }
+    }
+}
